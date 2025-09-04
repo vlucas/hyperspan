e04a54a86b9efc423570251d6d75f7d5543b361c
Vance Lucas <vance@vancelucas.com>
2025-09-03 22:53:07 -0500

    Hyperspan client: Issue w/streaming <form>s

--- packages/framework/src/clientjs/hyperspan-client.ts
+++ packages/framework/src/clientjs/hyperspan-client.ts
@@ -12,7 +12,7 @@ function htmlAsyncContentObserver() {
       const asyncContent = list
         .map((mutation) =>
           Array.from(mutation.addedNodes).find((node: any) => {
-            if (!node || !node?.id) {
+            if (!node || !node?.id || typeof node.id !== 'string') {
               return false;
             }
             return node.id?.startsWith('async_loading_') && node.id?.endsWith('_content');
